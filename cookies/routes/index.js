3bf93e5941e16cfc0d437b617a357c7f2cb12983
Ian Behrmann <ianjbehrmann@gmail.com>
2015-10-07 11:13:41 -0600

    basic ability to add / delete cookies

--- cookies/routes/index.js
+++ cookies/routes/index.js
@@ -3,12 +3,18 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.cookie('fraggle', req.query.foo)
+  res.render('index', { title: req.query.foo });
 });
 
 router.get('/show', function(req, res, next) {
-  res.render('index', { title: 'Show' });
+  res.render('index', { title: req.cookies.fraggle });
 });
+
+router.get('/delete', function (req, res, next){
+  res.render('index', { title: res.clearCookie('fraggle') });
+  res.redirect('/')
+})
 
 
 module.exports = router;
