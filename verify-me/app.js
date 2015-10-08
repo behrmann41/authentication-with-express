55fceefb6451c223f89626964120f068cba1d072
Ian Behrmann <ianjbehrmann@gmail.com>
2015-10-08 16:13:08 -0600

    encryption and sessions all functioning

--- verify-me/app.js
+++ verify-me/app.js
@@ -20,6 +20,10 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+  name: 'session',
+  keys: ['key1', 'key2']
+}))
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
--- verify-me/routes/index.js
+++ verify-me/routes/index.js
@@ -19,27 +19,65 @@ router.post('/register', function (req, res, next) {
   if (!req.body.email.trim()) {
     errors.push('Email cannot be blank')
   }
-  if (!req.body.password.trim()) {
-    errors.push('Password cannot be blank')
+  if (req.body.password.length < 8) {
+    errors.push('your password needs to be at least 8 characters')
+  }
+  if (req.body.password === 'password'){
+    errors.push('your password cannot be password')
   }
   if (req.body.password !== req.body.passconfirm){
     errors.push('passwords did not match')
   }
-  if (req.body.password.length < 8) {
-    errors.push('your password needs to be at least 8 characters')
-  }
   if (errors.length) {
     res.render('users/register', {title: 'Create an account', errors: errors})
   } else {
-    Users.insert({  user: req.body.email,
-                    passwordDigest: hash
-                })
-    res.redirect('/home')
+    Users.find({ user: req.body.email }, function (err, data){
+      if (data.length > 0) {
+        errors.push('email already in use')
+        res.render('users/register', {title: 'Create an account', errors: errors})
+      } else {
+        Users.insert({  user: req.body.email,
+                        passwordDigest: hash
+                    })
+        req.session.user = req.body.email
+        res.redirect('/home')
+      }
+    })
   }
 })
 
-router.get('/home', function (req, res, next){
-  res.render('users/home', { title: 'Homepage'})
+router.get('/login', function (req, res, next){
+  res.render('users/login', {  title: 'Sign In'})
 })
+
+router.post('/login', function (req, res, next){
+  var errors = []
+  Users.findOne({ user: req.body.email}, function (err, user){
+    if (user){
+      if (bcrypt.compareSync(req.body.password, user.passwordDigest)){
+        req.session.user = user.user
+        res.redirect('/home')
+      } else {
+        errors.push('Invalid Email/Password')
+        res.render('users/login', { title: 'Sign In', errors: errors})
+      }
+    } else {
+      errors.push('Invalid Email/Password')
+      res.render('users/login', { title: 'Sign In', errors: errors})
+    }
+  })
+})
+
+router.get('/home', function (req, res, next){
+  var username = req.session.user
+  res.render('users/home', { title: 'Homepage', user: username})
+})
+
+router.get('/logout', function (req, res, next){
+  req.session = null;
+  res.redirect('/')
+})
+
+
 
 module.exports = router;
